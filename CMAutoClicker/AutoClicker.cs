8017341916a8c4af9f1b3ad9eb984e58b77a3c21
erax0r <erax0r@gmail.com>
2015-02-14 18:07:52 -0800

    initial checkin

---
+++ CMAutoClicker/AutoClicker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+
+namespace CMAutoClicker
+{
+    public class AutoClicker
+    {
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public void ClickMouse(int x,int y, int duration, int interval)
+        {
+            var s = new System.Diagnostics.Stopwatch();
+            s.Start();
+            while (s.ElapsedMilliseconds < duration)
+            {
+                LeftMouseClick(x, y); //1920 x 1080 FullScreen windows
+                Thread.Sleep(interval);
+            }           
+        }       
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+    }
+}
---
+++ CMAutoClicker/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CMAutoClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AutoClicker autoClicker = new AutoClicker();
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            autoClicker.ClickMouse(Convert.ToInt32(this.xcoords.Text),Convert.ToInt32(this.ycoords.Text),Convert.ToInt32(this.duration.Text),Convert.ToInt32(this.interval.Text));
+        }
+
+    }
+}
